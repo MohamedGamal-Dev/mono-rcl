867bec716a8c39aecf4a4ba67456cb0129a618c9
Mohamed Gamal <MohamedGamal.Dv@gmail.com>
2022-09-29 08:15:57 +0200

    Refactor Button styles and add More variants

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,19 +1,46 @@
 import * as React from 'react';
+import './Button.css';
 
-type ButtonStyle = {
-  [property: string]: string;
-};
+// type ButtonStyle = {
+//   [property: string]: string;
+// };
 
 export interface ButtonProps {
   label: string;
-  style: ButtonStyle;
+  // style: ButtonStyle;
   onClick?: () => void;
+  variant?: string;
+  size?: 'small' | 'medium' | 'large';
+  backgroundColor?: string;
+  color?: string;
 }
 
-const Button: React.FunctionComponent<ButtonProps> = (props: ButtonProps) => {
+const Button: React.FunctionComponent<ButtonProps> = ({
+  label,
+  onClick,
+  variant = 'default',
+  size = 'medium',
+  backgroundColor,
+  color,
+  ...props
+}: ButtonProps) => {
+  switch (variant) {
+    case 'secondary':
+      'secondary';
+      break;
+    case 'danger':
+      'danger';
+      break;
+    default:
+  }
   return (
-    <button onClick={props.onClick} style={props.style}>
-      {props.label}
+    <button
+      className={['button', `button--${size}`, `button--${variant}`].join(' ')}
+      style={{ backgroundColor, color }}
+      onClick={onClick}
+      {...props}
+    >
+      {label}
     </button>
   );
 };
--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -6,33 +6,47 @@ import Button from '../components/Button';
 export default {
   title: 'Elements/Button',
   component: Button,
-  argTypes: { onClick: { action: 'clicked' } },
+  argTypes: {
+    onClick: { action: 'clicked' },
+    variant: { control: 'select', options: ['default', 'secondary', 'danger'] },
+    size: { control: 'radio', options: ['small', 'medium', 'large'] },
+  },
 } as ComponentMeta<typeof Button>;
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
-  label: 'Default button',
-  style: {
-    fontSize: '18px',
-    padding: '1em',
-    width: '10em',
-    margin: '0.8em',
-    color: 'white',
-    cursor: 'pointer',
-    border: '2px solid #16213E',
-    borderRadius: '5px',
-    background: '#16213E',
-  },
+  label: 'Button',
+  variant: 'default',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: 'Button',
+  variant: 'secondary',
 };
 
 export const Danger = Template.bind({});
 Danger.args = {
-  label: 'Danger',
-  style: {
-    ...Default.args.style,
-    border: '2px solid #E94560',
-    background: '#E94560',
-  }
+  label: 'Button',
+  variant: 'danger',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  label: 'Button',
+  size: 'small',
+};
+
+export const Medium = Template.bind({});
+Medium.args = {
+  label: 'Button',
+  size: 'medium',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  label: 'Button',
+  size: 'large',
 };
